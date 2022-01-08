0cc40b4ba9b2c61bee139468347586b5121d78a5
unknown <Quentinhnilica@gmail.com>
2022-01-08 10:10:38 -0500

    added workouts range get route

--- routes/api.js
+++ routes/api.js
@@ -14,4 +14,21 @@ router.get('/api/workouts', async (req, res) => {
     res.json(aggWorkout)
 })
 
+router.get('/api/workouts/range', async (req, res) => {
+    try {
+        const theWorkouts = await Workout.find({}).limit(7)
+
+        let allWorkout = theWorkouts.map(workout => {
+            let totalDuration = 0
+            workout.exercises.forEach(exercise => {
+                totalDuration += exercise.duration
+            })
+            return { ...workout._doc, totalDuration }
+        })
+        res.json(allWorkout)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 module.exports = router
