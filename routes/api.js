6811e0dbb4a6b6fce134b628972dce2002159e45
unknown <Quentinhnilica@gmail.com>
2022-01-18 03:52:29 -0500

    Finished the rest of the api routes

--- routes/api.js
+++ routes/api.js
@@ -31,4 +31,40 @@ router.get('/api/workouts/range', async (req, res) => {
     }
 })
 
+router.put('/api/workouts/:_id', async (req, res) => {
+
+    try {
+        const { _id } = req.params
+        const { type } = req.body
+        console.log(req.body)
+
+        if (type === 'cardio') {
+            const { distance, name, duration } = req.body
+            const newCardio = await Workout.findOneAndUpdate({ _id }, { $push: { exercises: { type, distance, name, duration } } }, { new: true, rawResult: true })
+            res.json(newCardio)
+        } else {
+            const { weight, sets, reps, name, duration } = req.body
+            const newResistance = await Workout.findOneAndUpdate({ _id }, { $push: { exercises: { type, weight, name, duration, sets, reps } } }, { new: true, rawResult: true })
+            res.json(newResistance)
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+router.post('/api/workouts', async (req, res) => {
+
+    try {
+        const newWorkout = await Workout.create({})
+        res.json(newWorkout)
+
+    } catch (err) {
+
+        res.status(500).json(err)
+
+    }
+
+})
+
 module.exports = router
