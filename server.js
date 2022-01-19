b8e484b4f17fec13995396f649212940ed6590e2
unknown <Quentinhnilica@gmail.com>
2022-01-19 00:55:36 -0500

    Changed to my new database

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstrackerdb", 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/myFirstDatabase", 
 {
     autoIndex: false,
     useNewUrlParser: true, 
